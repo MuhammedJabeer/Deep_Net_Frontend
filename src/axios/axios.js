87f60c6793340aa74929d214528aaf96e82452e9
MuhammedJabeer <jabeerjabeer1111@gmail.com>
2025-06-19 11:31:18 +0530

    Third commit

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001", 
+  baseURL: "https://deep-net-backend.onrender.com", 
   headers: {
     "Content-Type": "application/json",
   },
